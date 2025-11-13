aabe0d728921759c1593dfbc7052953d2e3208e4
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2025-11-13 00:31:50 -0500

    practicing for loops in python again.

--- AddPythonPracticeFiles.py/L17CompoundIntCalc.py
+++ AddPythonPracticeFiles.py/L17CompoundIntCalc.py
@@ -12,23 +12,23 @@ time = 0
 while True:
     principle = float(input("Enter the principle amount: "))
     if principle < 0:
-        print("Principle can't be less than zero")
+        print("Principle can't be less than or equal to zero")
     else:
         break
 
 while True:
     rate = float(input("Enter the interest rate: "))
     if rate < 0:
-        print("Interest rate can't be less than zero")
+        print("Interest rate can't be less than or equal to zero")
     else:
         break
 
 while True:
     time = int(input("Enter the time in years: "))
     if time < 0:
-        print("Time can't be less than zero")
+        print("Time can't be less than or equal to zero")
     else:
         break
 
 total = principle * pow((1 + rate / 100 ), time)
-print(f"Balance after {time} years/s:${total:.2f}")
+print(f"Balance after {time} year/s:$ {total:.2f}")
---
+++ AddPythonPracticeFiles.py/L18ForLoopPrac.py
@@ -0,0 +1,19 @@
+# focusing on For loops
+# for loops = execute a block of code a fixed number of times.
+# you can iterate over a range, str, sequence, ect.
+# basic syntax
+#for i in range(1, 11):
+#    print(i)
+#creditCardNum = "1234-3456-6879-5670"
+#for i in creditCardNum:
+#    print(i)
+    
+#print("Happy new year!")
+# or practice with while loops:
+for i in range(1 , 21):
+    if i == 13:
+        # continue keyword used to go to the next item
+        #continue
+        break # and break is used to stop it
+    else:
+        print(i)
