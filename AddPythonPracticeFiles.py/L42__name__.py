d0a018a765dba6362a3ae6f566fd3c46565f0444
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2025-12-06 00:25:19 -0500

    practice to focus on dunder naming convention in Py

---
+++ AddPythonPracticeFiles.py/L42__name__.py
@@ -0,0 +1,12 @@
+# if __name__ == __main__: (this script can be import OR run standalone)
+#                           Functions and classes in this module can be reused
+#                           without the main block of code excuting
+
+# ex. library = Importing library for functionality
+#               When running library directly, display a help page
+def main():
+    # your program goes here
+    pass
+
+if __name__ == '__main__':
+    main()
